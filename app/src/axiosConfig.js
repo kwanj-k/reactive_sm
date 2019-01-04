6a34972efdccf53269c37e31746aad0bdaf517a5
Kelvin Mwangi <33450849+kwanj-k@users.noreply.github.com>
2019-01-04 21:31:21 +0300

    Develop (#5)
    * Add authenitication
    Consume login endpoint
    Add token to the products headers
    
    * .

--- app/src/axiosConfig.js
+++ app/src/axiosConfig.js
@@ -5,7 +5,7 @@ const BASE_URL = "http://127.0.0.1:8000";
 const axiosConfig = axios.create({
     baseURL: BASE_URL,
     headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
     },
 });
 export default axiosConfig;
